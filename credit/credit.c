a57f7b98f9e8d4bd7ad8c9e2dbc97c75ac3e5ba4
toluboltz <toluboltz@gmail.com>
2022-04-06 21:13:33 +0000

    Wed, Apr 6, 2022, 9:13 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,12 +5,11 @@ int main(void)
 {
     long int credit_number = 4003600000000014;
 
-    int modulus = credit_number % 10;
-    int divide = credit_number / 10;
+    int modulus;
 
     int i = 0;
     while (true)
     {
-        
+        modulus = 
     }
 }

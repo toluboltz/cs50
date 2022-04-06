a66725453b73be39b383b05d92bf76cc2da5b8d1
toluboltz <toluboltz@gmail.com>
2022-04-06 19:33:27 +0000

    Wed, Apr 6, 2022, 7:33 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -6,16 +6,14 @@ int main(void)
     int height;
     string gap = "  ";
 
-    // Get the height of the pyramids.
-    // Height must be between 1 and 8 inclusive.
-    do
-    {
-        height = get_int("Height: ");
-    }
-    while(height < 1 || height > 8);
+    // // Get the height of the pyramids.
+    // // Height must be between 1 and 8 inclusive.
+    // do
+    // {
+    //     height = get_int("Height: ");
+    // }
+    // while(height < 1 || height > 8);
 
-    for (int i = 0; i < height; i++)
-    {
-        
-    }
+    printf(" #  # \n");
+    printf("##  ##\n");
 }

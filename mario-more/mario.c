af5c81f670fd2657b298161f71f8e99190c6304d
toluboltz <toluboltz@gmail.com>
2022-04-06 20:00:12 +0000

    Wed, Apr 6, 2022, 8:00 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -15,7 +15,7 @@ int main(void)
         height = get_int("Height: ");
     }
     while(height < 1 || height > 8);
-
+    
     pyramid(height, space_length);
 }
 
@@ -23,22 +23,27 @@ void pyramid(int height, int space_length)
 {
     for (int i = 1; i <= height; i++)
     {
+        // Print preceding spaces
         for (int j = height - i; j > 0; j--)
         {
             printf(" ");
         }
+        // print left #'s
         for (int k = 0; k < i; k++)
         {
             printf("#");
         }
+        // print "gap" spaces
         for (int s = 0; s < space_length; s++)
         {
             printf(" ");
         }
+        // print right #'s
         for (int k = 0; k < i; k++)
         {
             printf("#");
         }
+        // go to the next row
         printf("\n");
     }
 }

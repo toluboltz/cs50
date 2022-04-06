343848ea14ce9aa32157aa5917d61dd0443c2097
toluboltz <toluboltz@gmail.com>
2022-04-06 19:10:21 +0000

    Wed, Apr 6, 2022, 7:10 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    do
+    {
+        int height = get_int("Height: ");
+    }
+    while(height > 0 && height < 9);
 }

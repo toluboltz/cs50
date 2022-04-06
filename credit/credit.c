fc4dab096d32c73f8efaac9bdae3af2565c7285b
toluboltz <toluboltz@gmail.com>
2022-04-06 21:18:45 +0000

    Wed, Apr 6, 2022, 9:18 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,13 +3,24 @@
 
 int main(void)
 {
-    long int credit_number = 4003600000000014;
+    long int card_number = 4003600000000014;
 
     int modulus;
 
-    int i = 0;
+    int i = 1;
     while (true)
     {
-        modulus = 
+        modulus = card_number % 10;
+        printf("Modulus %i: %i\n", i, modulus);
+        card_number -= card_number - modulus;
+        printf("Card number %i: %li\n", i, card_number);
+
+        if (card_number == 0)
+        {
+            break;
+        }
+
+        i++;
     }
+    printf("Done\n");
 }

4ce44de08fe3b646bf90d414f2e87d5d73bd3846
toluboltz <toluboltz@gmail.com>
2022-04-07 01:08:28 +0000

    Thu, Apr 7, 2022, 1:08 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -34,7 +34,7 @@ int main(void)
                 {
                     break;
                 }
-                product_sum += product
+                product_sum += remainder
             }
 
 

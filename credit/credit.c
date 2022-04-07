99b7037397f3150e2af544fa79a9db3f0a8e7e6c
toluboltz <toluboltz@gmail.com>
2022-04-07 01:42:18 +0000

    Thu, Apr 7, 2022, 1:42 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -29,20 +29,16 @@ int check_sum(long int card_number)
         // operation to retrieve the last digit
         int modulus = card_number % 10;
 
-        // check for other digits
-        if (i % 2 == 0)
-        {
-            other_sum += modulus;
-        }
         // check for every other digit starting from
         // second-to-last
-        else
+        if (i % 2 != 0)
         {
+            // multiply each digit by 2
             int product = modulus * 2;
-            int remainder;
             while (true)
             {
-                remainder = product % 10;
+                //
+                int remainder = product % 10;
                 product_sum += remainder;
 
                 product = (product - remainder) / 10;
@@ -52,6 +48,13 @@ int check_sum(long int card_number)
                     break;
                 }
             }
+
+        }
+        // check for other digits
+        else
+        {
+            // and sum them up
+            other_sum += modulus;
         }
 
         card_number = (card_number - modulus) / 10;

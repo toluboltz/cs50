bd47b3cbfbae543b5c12e20f2b71a49e33a2b078
toluboltz <toluboltz@gmail.com>
2022-04-19 15:58:58 +0000

    Tue, Apr 19, 2022, 3:58 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -24,14 +24,12 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    // Convert the word to uppercase
-    word = word.upper()
     // get the length of the word
     int len = strlen(word);
 
     for (int i = 0; i < len; i++)
     {
         // Convert each letter to uppercase
-        char letter =
+        char letter = 
     }
 }

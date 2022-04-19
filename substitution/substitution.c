1112c750802a83d83fd86081c94e007337b7af38
toluboltz <toluboltz@gmail.com>
2022-04-19 16:28:45 +0000

    Tue, Apr 19, 2022, 4:28 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,7 +3,12 @@
 
 int main(int argc, string argv[])
 {
-    // Validate the input key
+    // Validate the input
+    // Check for correct number of command-line arguments
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key\n");
+    }
     // For uniformity, convert the key to uppercase
     key_
 }

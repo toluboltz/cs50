887750d0f172fb3fe95504eee797c9039af41a1e
toluboltz <toluboltz@gmail.com>
2022-04-20 00:11:19 +0000

    Wed, Apr 20, 2022, 12:11 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,10 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int count_letters(string text);
+int count_letters(string);
 
 int main(void)
 {
     // Prompt user for text
     string text = get_string("Text: ");
+
+    number_of_letters = count_letters(text);
+}
+
+int count_letters(string text)
+{
+    
 }

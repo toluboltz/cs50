b99f1cfe89a0e0e99e5265508040a2125395261e
toluboltz <toluboltz@gmail.com>
2022-04-23 09:58:41 +0000

    Sat, Apr 23, 2022, 9:58 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,10 +36,10 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
     fread(header, sizeof(uint8_t), HEADER_SIZE, input);
-    fwrite()
-
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
+    
 
     // Close files
     fclose(input);

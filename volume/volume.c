67842129a6c2ff01c3351b6cc4b8d1f77a458cdd
toluboltz <toluboltz@gmail.com>
2022-04-23 10:13:57 +0000

    Sat, Apr 23, 2022, 10:13 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -42,6 +42,7 @@ int main(int argc, char *argv[])
     int16_t buffer;
     while (fread(&buffer, sizeof(int16_t), 1, input) == 1)
     {
+        buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
     }
 

a92756c3f2e32d59abeada23f7f161dd14513359
toluboltz <toluboltz@gmail.com>
2022-04-25 16:04:10 +0000

    Mon, Apr 25, 2022, 4:04 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -42,12 +42,6 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
     for (int i = 0; i < height; i++)
     {
-        int sum_red = 0;
-        int sum_green = 0;
-        int sum_blue = 0;
-
-        int pixel_count = 0;
-
         for (int j = 0; j < width; j++)
         {
             // Initialize the indices of all pixels
@@ -61,11 +55,14 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             int bottom_middle[] = {i + 1, j};
             int bottom_right[] = {i + 1, j + 1};
 
-            sum_red += image[i][j].rgbtRed;
-            sum_red += image[i][j].rgbtGreen;
-            sum_red += image[i][j].rgbtBlue;
-            pixel_count += 1;
+            // middle-pixel
+            // Initialize sum for each color and pixel count
+            int sum_red = image[i][j].rgbtRed;
+            int sum_blue = image[i][j].rgbtGreen;
+            int sum_green = image[i][j].rgbtBlue;
+            int pixel_count = 1;
 
+            // top-left
             if (top_left[0] >= 0 && top_left[1] >= 0)
             {
                 sum_red += image[top_left[0]][top_left[1]].rgbtRed;
@@ -74,6 +71,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 pixel_count += 1;
 
             }
+            // top-middle
             if (top_middle[0] >= 0)
             {
                 sum_red += image[top_middle[0]][top_middle[1]].rgbtRed;
@@ -81,6 +79,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 sum_blue += image[top_middle[0]][top_middle[1]].rgbtBlue;
                 pixel_count += 1;
             }
+            // top-right
             if (top_right[0] >= 0 && top_right[1] < width)
             {
                 sum_red += image[top_right[0]][top_right[1]].rgbtRed;
@@ -88,6 +87,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 sum_blue += image[top_right[0]][top_right[1]].rgbtBlue;
                 pixel_count += 1;
             }
+            // middle-left
             if (middle_left[1] >= 0)
             {
                 sum_red += image[middle_left[0]][middle_left[1]].rgbtRed;
@@ -95,6 +95,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 sum_blue += image[middle_left[0]][middle_left[1]].rgbtBlue;
                 pixel_count += 1;
             }
+            // middle-right
             if (middle_right[1] < width)
             {
                 sum_red += image[middle_right[0]][middle_right[1]].rgbtRed;
@@ -102,6 +103,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 sum_blue += image[middle_right[0]][middle_right[1]].rgbtBlue;
                 pixel_count += 1;
             }
+            // bottom-left
             if (bottom_left[0] < height && bottom_left[1] >= 0)
             {
                 sum_red += image[bottom_left[0]][bottom_left[1]].rgbtRed;
@@ -109,6 +111,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 sum_blue += image[bottom_left[0]][bottom_left[1]].rgbtBlue;
                 pixel_count += 1;
             }
+            // bottom-middle
             if (bottom_middle[0] < height)
             {
                 sum_red += image[bottom_middle[0]][bottom_middle[1]].rgbtRed;
@@ -116,6 +119,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 sum_blue += image[bottom_middle[0]][bottom_middle[1]].rgbtBlue;
                 pixel_count += 1;
             }
+            // bottom-right
             if (bottom_right[0] < height && bottom_right[1] < width)
             {
                 sum_red += image[bottom_right[0]][bottom_right[1]].rgbtRed;
@@ -123,7 +127,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 sum_blue += image[bottom_right[0]][bottom_right[1]].rgbtBlue;
                 pixel_count += 1;
             }
-
+            // compute average for each color
             image[i][j].rgbtRed = round(sum_red * 1.0 / pixel_count);
             image[i][j].rgbtGreen = round(sum_green * 1.0 / pixel_count);
             image[i][j].rgbtBlue = round(sum_blue * 1.0 / pixel_count);

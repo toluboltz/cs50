ba3360052d8e81ba8a71942d5503238f99f407a0
toluboltz <toluboltz@gmail.com>
2022-04-25 16:20:54 +0000

    Mon, Apr 25, 2022, 4:20 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -39,7 +39,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE image_copy = 
+    // Make a copy of the image
+    RGBTRIPLE image_copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image_copy[i][j] = image[i][j];
+        }
+    }
+    // Blur the image
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -57,74 +66,74 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
             // middle-pixel
             // Initialize sum for each color and pixel count
-            int sum_red = image[i][j].rgbtRed;
-            int sum_blue = image[i][j].rgbtGreen;
-            int sum_green = image[i][j].rgbtBlue;
+            int sum_red = image_copy[i][j].rgbtRed;
+            int sum_blue = image_copy[i][j].rgbtGreen;
+            int sum_green = image_copy[i][j].rgbtBlue;
             int pixel_count = 1;
 
             // top-left
             if (top_left[0] >= 0 && top_left[1] >= 0)
             {
-                sum_red += image[top_left[0]][top_left[1]].rgbtRed;
-                sum_green += image[top_left[0]][top_left[1]].rgbtGreen;
-                sum_blue += image[top_left[0]][top_left[1]].rgbtBlue;
+                sum_red += image_copy[top_left[0]][top_left[1]].rgbtRed;
+                sum_green += image_copy[top_left[0]][top_left[1]].rgbtGreen;
+                sum_blue += image_copy[top_left[0]][top_left[1]].rgbtBlue;
                 pixel_count += 1;
 
             }
             // top-middle
             if (top_middle[0] >= 0)
             {
-                sum_red += image[top_middle[0]][top_middle[1]].rgbtRed;
-                sum_green += image[top_middle[0]][top_middle[1]].rgbtGreen;
-                sum_blue += image[top_middle[0]][top_middle[1]].rgbtBlue;
+                sum_red += image_copy[top_middle[0]][top_middle[1]].rgbtRed;
+                sum_green += image_copy[top_middle[0]][top_middle[1]].rgbtGreen;
+                sum_blue += image_copy[top_middle[0]][top_middle[1]].rgbtBlue;
                 pixel_count += 1;
             }
             // top-right
             if (top_right[0] >= 0 && top_right[1] < width)
             {
-                sum_red += image[top_right[0]][top_right[1]].rgbtRed;
-                sum_green += image[top_right[0]][top_right[1]].rgbtGreen;
-                sum_blue += image[top_right[0]][top_right[1]].rgbtBlue;
+                sum_red += image_copy[top_right[0]][top_right[1]].rgbtRed;
+                sum_green += image_copy[top_right[0]][top_right[1]].rgbtGreen;
+                sum_blue += image_copy[top_right[0]][top_right[1]].rgbtBlue;
                 pixel_count += 1;
             }
             // middle-left
             if (middle_left[1] >= 0)
             {
-                sum_red += image[middle_left[0]][middle_left[1]].rgbtRed;
-                sum_green += image[middle_left[0]][middle_left[1]].rgbtGreen;
-                sum_blue += image[middle_left[0]][middle_left[1]].rgbtBlue;
+                sum_red += image_copy[middle_left[0]][middle_left[1]].rgbtRed;
+                sum_green += image_copy[middle_left[0]][middle_left[1]].rgbtGreen;
+                sum_blue += image_copy[middle_left[0]][middle_left[1]].rgbtBlue;
                 pixel_count += 1;
             }
             // middle-right
             if (middle_right[1] < width)
             {
-                sum_red += image[middle_right[0]][middle_right[1]].rgbtRed;
-                sum_green += image[middle_right[0]][middle_right[1]].rgbtGreen;
-                sum_blue += image[middle_right[0]][middle_right[1]].rgbtBlue;
+                sum_red += image_copy[middle_right[0]][middle_right[1]].rgbtRed;
+                sum_green += image_copy[middle_right[0]][middle_right[1]].rgbtGreen;
+                sum_blue += image_copy[middle_right[0]][middle_right[1]].rgbtBlue;
                 pixel_count += 1;
             }
             // bottom-left
             if (bottom_left[0] < height && bottom_left[1] >= 0)
             {
-                sum_red += image[bottom_left[0]][bottom_left[1]].rgbtRed;
-                sum_green += image[bottom_left[0]][bottom_left[1]].rgbtGreen;
-                sum_blue += image[bottom_left[0]][bottom_left[1]].rgbtBlue;
+                sum_red += image_copy[bottom_left[0]][bottom_left[1]].rgbtRed;
+                sum_green += image_copy[bottom_left[0]][bottom_left[1]].rgbtGreen;
+                sum_blue += image_copy[bottom_left[0]][bottom_left[1]].rgbtBlue;
                 pixel_count += 1;
             }
             // bottom-middle
             if (bottom_middle[0] < height)
             {
-                sum_red += image[bottom_middle[0]][bottom_middle[1]].rgbtRed;
-                sum_green += image[bottom_middle[0]][bottom_middle[1]].rgbtGreen;
-                sum_blue += image[bottom_middle[0]][bottom_middle[1]].rgbtBlue;
+                sum_red += image_copy[bottom_middle[0]][bottom_middle[1]].rgbtRed;
+                sum_green += image_copy[bottom_middle[0]][bottom_middle[1]].rgbtGreen;
+                sum_blue += image_copy[bottom_middle[0]][bottom_middle[1]].rgbtBlue;
                 pixel_count += 1;
             }
             // bottom-right
             if (bottom_right[0] < height && bottom_right[1] < width)
             {
-                sum_red += image[bottom_right[0]][bottom_right[1]].rgbtRed;
-                sum_green += image[bottom_right[0]][bottom_right[1]].rgbtGreen;
-                sum_blue += image[bottom_right[0]][bottom_right[1]].rgbtBlue;
+                sum_red += image_copy[bottom_right[0]][bottom_right[1]].rgbtRed;
+                sum_green += image_copy[bottom_right[0]][bottom_right[1]].rgbtGreen;
+                sum_blue += image_copy[bottom_right[0]][bottom_right[1]].rgbtBlue;
                 pixel_count += 1;
             }
             // compute average for each color
@@ -138,5 +147,14 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // Make a copy of the image
+    RGBTRIPLE image_copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image_copy[i][j] = image[i][j];
+        }
+    }
+    
 }

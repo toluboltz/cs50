cd386fe71db284ef65b2fcf755bb31512f3ccf9a
toluboltz <toluboltz@gmail.com>
2022-04-25 14:27:54 +0000

    Mon, Apr 25, 2022, 2:27 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,5 +1,6 @@
 #include "helpers.h"
 #include <stdio.h>
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -8,6 +9,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            image[i][j] = 
             printf("R: %hhu, G: %hhu, B: %hhu ", image[i][j].rgbtRed, image[i][j].rgbtGreen, image[i][j].rgbtBlue);
         }
         printf("\n");

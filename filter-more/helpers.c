51177b4c2f1c69832b177b1f1174c79537810c44
toluboltz <toluboltz@gmail.com>
2022-04-25 12:18:50 +0000

    Mon, Apr 25, 2022, 12:18 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            printf("%i)
+            printf("%zu)
         }
     }
 }

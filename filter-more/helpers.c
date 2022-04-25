5cd3de0526df29e3a4f47ef48d907b787275e745
toluboltz <toluboltz@gmail.com>
2022-04-25 15:42:14 +0000

    Mon, Apr 25, 2022, 3:42 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -42,14 +42,61 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
     for (int i = 0; i < height; i++)
     {
+        int sum_red = 0;
+        int sum_green = 0;
+        int sum_blue = 0;
+
+        int pixel_count = 0;
+
         for (int j = 0; j < width; j++)
         {
             // Initialize the indices of all pixels
-        // within one row and column of the original pixel
-        int top_left[][] = {i - 1, j - 1};
-        int middle_left[][] = {i, j - 1};
-        int bottom_left[][] = {i + 1; j -1};
-        int 
+            // within one row and column of the original pixel
+            int top_left[] = {i - 1, j - 1};
+            int top_middle[] = {i - 1, j};
+            int top_right[] = {i - 1, j + 1};
+            int middle_left[]= {i, j - 1};
+            int middle_right[] = {i, j + 1};
+            int bottom_left[] = {i + 1, j -1};
+            int bottom_middle[] = {i + 1, j};
+            int bottom_right[] = {i + 1, j + 1};
+
+            if (top_left[0] >= 0 && top_left[1] >= 0)
+            {
+                sum_red += image[top_left[0]][top_left[1]].rgbtRed;
+                sum_green += image[top_left[0]][top_left[1]].rgbtGreen;
+                sum_blue += image[top_left[0]][top_left[1]].rgbtBlue;
+                pixel_count += 1;
+
+            }
+            if (top_middle[0] >= 0)
+            {
+                sum_red += image[top_middle[0]][top_middle[1]].rgbtRed;
+                sum_green += image[top_middle[0]][top_middle[1]].rgbtGreen;
+                sum_blue += image[top_middle[0]][top_middle[1]].rgbtBlue;
+                pixel_count += 1;
+            }
+            if (top_right[0] >= 0 && top_right[1] < width)
+            {
+                sum_red += image[top_right[0]][top_right[1]].rgbtRed;
+                sum_green += image[top_right[0]][top_right[1]].rgbtGreen;
+                sum_blue += image[top_right[0]][top_right[1]].rgbtBlue;
+                pixel_count += 1;
+            }
+            if (middle_left[1] >= 0)
+            {
+                sum_red += image[middle_left[0]][middle_left[1]].rgbtRed;
+                sum_green += image[middle_left[0]][middle_left[1]].rgbtGreen;
+                sum_blue += image[middle_left[0]][middle_left[1]].rgbtBlue;
+                pixel_count += 1;
+            }
+            if (middle_right[1] < width)
+            {
+                sum_red += image[middle_right[0]][middle_right[1]].rgbtRed;
+                sum_green += image[middle_right[0]][middle_right[1]].rgbtGreen;
+                sum_blue += image[middle_right[0]][middle_right[1]].rgbtBlue;
+                pixel_count += 1;
+            }
         }
     }
 }

162cb68ff8c19ab6d1bbf3b1abc8e58fb9fc135d
toluboltz <toluboltz@gmail.com>
2022-04-26 18:04:55 +0000

    Tue, Apr 26, 2022, 6:04 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -37,7 +37,10 @@ int main(int argc, char *argv[])
     {
         for (int i = 0; i < 512; i++)
         {
-            printf("%hhu ", jpg[i]);
+            if (jpg[0] == 0xff && jpg[1] == 0xd8 && jpg[2] == 0xff && (jpg[3] & 0xf0 == 0xe0))
+            {
+                printf("%hhu ", jpg[i]);
+            }
         }
         printf("\n\n");
         // if (jpg[0] == 0xff && jpg[1] == 0xd8 && jpg[2] == 0xff && )

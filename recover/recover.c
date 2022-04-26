b1d12dc10b262d3a24a869863b9222c5c4b3936c
toluboltz <toluboltz@gmail.com>
2022-04-26 17:23:05 +0000

    Tue, Apr 26, 2022, 5:23 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
     printf("here\n");
     while (fread(jpg, sizeof(BYTE), BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
-        printf("%s\n", jpg[0]);
+        printf("%hhu\n", jpg[0]);
         // if (jpg[0] == 0xff && jpg[1] == 0xd8 && jpg[2] == 0xff && )
         // {
 

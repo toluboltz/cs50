f08640ca9d6045a4ce7d58455869088325681bbf
toluboltz <toluboltz@gmail.com>
2022-04-26 17:30:45 +0000

    Tue, Apr 26, 2022, 5:30 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -35,9 +35,9 @@ int main(int argc, char *argv[])
     printf("here\n");
     while (fread(jpg, sizeof(BYTE), BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
-        if (jpg[0] == 0xff)
+        for (int i = 0; i < 512; i++)
         {
-            printf("True\n");
+            printf("%hhu\n", jpg[i]);
         }
         // if (jpg[0] == 0xff && jpg[1] == 0xd8 && jpg[2] == 0xff && )
         // {

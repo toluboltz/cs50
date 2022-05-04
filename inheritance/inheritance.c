d4f99d2f7a1ebbaa22fee1b6b15ba09cbdacffe9
toluboltz <toluboltz@gmail.com>
2022-05-04 14:27:47 +0000

    Wed, May 4, 2022, 2:27 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -58,6 +58,7 @@ person *create_family(int generations)
         n->parent[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        n->alleles[0] = n->parentsrand() % 2
 
     }
 

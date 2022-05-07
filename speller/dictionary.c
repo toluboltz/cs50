9105b1187a27bdbfb451f70a90cb4064f66993ce
toluboltz <toluboltz@gmail.com>
2022-05-07 16:51:20 +0000

    Sat, May 7, 2022, 4:51 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -168,11 +168,11 @@ void free_dictionary(trie *trav)
     // Loop through the arrays of each node
     for (int i = 0; i < N; i++)
     {
-        
+
         // Check if array points to another node
-        if (n->next_letter[i] != NULL)
+        if (trav->next_letter[i] != NULL)
         {
-            free_dictionary(n->next_letter[i]);
+            free_dictionary(trav->next_letter[i]);
         }
     }
 

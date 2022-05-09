3457d01e5c8ae090e8259af7ebe3c9ad71b9f047
toluboltz <toluboltz@gmail.com>
2022-05-09 01:12:10 +0000

    Mon, May 9, 2022, 1:12 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -56,7 +56,8 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    
+
+    simulate_round(teams)
 
 
 if __name__ == "__main__":

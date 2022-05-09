df718bf334de1e7448126ba73a45c358165e20ff
toluboltz <toluboltz@gmail.com>
2022-05-09 01:08:25 +0000

    Mon, May 9, 2022, 1:08 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,13 +16,15 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    with open(argv[1], "r") as file:
+    with open(sys.argv[1], "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
             teams.append(row)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
+    for i in range(N):
+
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):

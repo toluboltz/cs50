98386c5fe4a97d6c955b4d044060c16c21b47146
toluboltz <toluboltz@gmail.com>
2022-05-09 03:00:43 +0000

    Mon, May 9, 2022, 3:00 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -5,8 +5,21 @@ from cs50 import get_int
 
 def main():
 
+    # Get card number
+    card_number = get_int("Number: ")
+
+    sum = check_sum(card_number)
+
+    card_type = verify(sum, card_number)
+
 
 def check_sum(card_number):
+    pass
 
 
-def verify()
+def verify(sum, card_number):
+    
+
+
+if __name__ == "__main__":
+    main()

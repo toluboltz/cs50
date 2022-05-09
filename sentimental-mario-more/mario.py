9b5ea99d28f9004d589a15dea34e99c3f6caeada
toluboltz <toluboltz@gmail.com>
2022-05-09 02:27:36 +0000

    Mon, May 9, 2022, 2:27 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,5 +1,14 @@
 # Program that prints out double half-pyramid of a specified height
 
+from cs50 import get_int
+
+
 def main():
 
-    # Get 
+    # Get half-pyramid height, must be betweeen 1 and 8 inclusive
+    while True:
+        height = get_int("Height: ")
+        if height >= 1 and height <= 8:
+            break
+
+    

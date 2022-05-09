b8caf9db6090bdc64d60523dfd5a217ff217df4c
toluboltz <toluboltz@gmail.com>
2022-05-09 02:10:23 +0000

    Mon, May 9, 2022, 2:10 AM +00:00

--- sentimental-hello/hello.py
+++ sentimental-hello/hello.py
@@ -1,6 +1,10 @@
 # Program that asks user for their name and prints out "hello, <name>"
 
 def main():
-    """
+    """Request user's name and print out hello, <name>."""
     name = input("What is your name? ")
-    prin
+    print(f"hello, {name}")
+
+
+if __name__ == "__main__":
+    main()

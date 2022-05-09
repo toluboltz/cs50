cecddb76a0e3478cea1fdd9d82a326f0ddf8d359
toluboltz <toluboltz@gmail.com>
2022-05-09 00:52:51 +0000

    Mon, May 9, 2022, 12:52 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,6 +16,8 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
+    with open(argv[1], "r") as file:
+        
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts

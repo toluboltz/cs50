0b9c899da5c1b18eda7b0dc3e5d94c2035e9d80d
toluboltz <toluboltz@gmail.com>
2022-05-09 00:55:12 +0000

    Mon, May 9, 2022, 12:55 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -17,7 +17,9 @@ def main():
     teams = []
     # TODO: Read teams into memory from file
     with open(argv[1], "r") as file:
-        
+        reader = csv.DictReader(file)
+        for row in reader:
+            
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts

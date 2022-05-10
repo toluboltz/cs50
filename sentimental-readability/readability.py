6ad9c06831e4caee2e8863025301bba47b700eb6
toluboltz <toluboltz@gmail.com>
2022-05-10 00:09:30 +0000

    Tue, May 10, 2022, 12:09 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,2 +1,15 @@
 # Program that computes the approximate grade level needed to comprehend some text
 
+def main():
+    pass
+
+
+def count_letters(text):
+    pass
+
+
+def count_words(text):
+    pass
+
+
+def count

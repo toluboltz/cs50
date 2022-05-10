f4f162ca3464361dc4e3e92cbbb16e79bbfe5389
toluboltz <toluboltz@gmail.com>
2022-05-10 00:35:47 +0000

    Tue, May 10, 2022, 12:35 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -8,14 +8,10 @@ def main():
     # Get text from user
     text = input("Text: ").upper()
 
-    # Count number of letters in text
-    letter_count = count_letters(text)
+    # Count number of letters, words and sentences
+    letter, word, sentence = count(text)
 
-    # Count number of words in text
-    word_count = count_words(text)
-
-    # Count number of sentences in text
-    sentence_count = count_sentences(text)
+    print(letter, word, sentence)
 
 
 def count(text):
@@ -26,32 +22,17 @@ def count(text):
 
     letter_count = 0
     word_count = 1
-    sentence_count = 1
+    sentence_count = 0
 
-
-def count_letters(text):
-    """Count letters in text. Return number of letters."""
-    p = re.compile(r'[A-Z]')
-
-    count = 0
     for l in text:
-        if (p.match(l)):
-            count += 1
+        if (letter_count.match(l)):
+            letter_count += 1
+        elif (word_count.match(l)):
+            word_count += 1
+        elif (sentence_count.match(l)):
+            sentence_count += 1
 
-    return count
-
-
-def count_words(text):
-    """Count words in text. Return number of words."""
-    p = re.comile(r'[ ]')
-
-    count = 1
-    for
-
-
-def count_sentences(text):
-    """Count sentences in text. Return number of sentences."""
-    pass
+    return letter_count, word_count, sentence_count
 
 
 if __name__ == "__main__":

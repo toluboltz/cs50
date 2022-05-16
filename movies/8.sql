b3ac46a70f8ce806374992517bb86104134f2d6c
toluboltz <toluboltz@gmail.com>
2022-05-16 10:31:26 +0000

    Mon, May 16, 2022, 10:31 AM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,3 +1,5 @@
 SELECT name FROM people
-WHERE id IN (SELECT stars.person_id FROM stars
-             JOIN movies)
+WHERE id IN (
+SELECT stars.person_id FROM stars
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.title = 'Toy Story');

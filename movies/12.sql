3b3728fc8be686d033d21d1d9032175fd01c7e44
toluboltz <toluboltz@gmail.com>
2022-05-16 11:06:00 +0000

    Mon, May 16, 2022, 11:06 AM +00:00

---
+++ movies/12.sql
@@ -0,0 +1,4 @@
+SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON people.id = stars.person_id
+WHERE people.name = 'Johnny Depp' OR people.name = 'Helena Bonham Carter';

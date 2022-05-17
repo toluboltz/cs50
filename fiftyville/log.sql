4a4d84acfef235a050a8b27e823ad217b2f47037
toluboltz <toluboltz@gmail.com>
2022-05-17 23:49:27 +0000

    Tue, May 17, 2022, 11:49 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -263,3 +263,74 @@ AND caller = (
 );
 
 -- Get the accomplice from the people table using the receiver phone number
+SELECT *
+FROM people
+WHERE phone_number = (
+    SELECT receiver
+    FROM phone_calls
+    WHERE month = 7
+    AND day = 28
+    AND duration < 60
+    AND caller = (
+        SELECT phone_number
+        FROM people
+        WHERE id IN (
+            SELECT person_id
+            FROM bank_accounts
+            WHERE account_number
+            IN (
+                SELECT account_number
+                FROM atm_transactions
+                WHERE month = 7
+                AND day = 28
+                AND transaction_type = 'withdraw'
+                AND atm_location = 'Leggett Street'
+            )
+        )
+        AND phone_number IN (
+            SELECT caller
+            FROM phone_calls
+            WHERE month = 7
+            AND day = 28
+            AND duration < 60
+        )
+        AND passport_number IN (
+            SELECT passport_number
+            FROM passengers
+            WHERE flight_id
+            IN (
+                SELECT id
+                FROM flights
+                WHERE month = 7
+                AND day = 29
+                AND origin_airport_id = (
+                    SELECT id
+                    FROM airports
+                    WHERE city = 'Fiftyville'
+                )
+                AND destination_airport_id = (
+                    SELECT destination_airport_id
+                    FROM flights
+                    WHERE month = 7
+                    AND day = 29
+                    AND origin_airport_id = (
+                        SELECT id
+                        FROM airports
+                        WHERE city = 'Fiftyville'
+                    )
+                    ORDER BY hour, minute
+                    LIMIT 1
+                )
+            )
+        )
+        AND license_plate IN (
+            SELECT license_plate
+            FROM bakery_security_logs
+            WHERE activity = 'exit'
+            AND month = 7
+            AND hour = 10
+            AND minute >= 5
+            AND minute <= 25
+        )
+    )
+);

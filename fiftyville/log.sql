049a67d8d242d34cd893553f826f58d76eedf48d
toluboltz <toluboltz@gmail.com>
2022-05-17 23:37:58 +0000

    Tue, May 17, 2022, 11:37 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -148,6 +148,19 @@ AND passport_number IN (
             FROM airports
             WHERE city = 'Fiftyville'
         )
+        AND destination_airport_id = (
+            SELECT destination_airport_id
+            FROM flights
+            WHERE month = 7
+            AND day = 29
+            AND origin_airport_id = (
+                SELECT id
+                FROM airports
+                WHERE city = 'Fiftyville'
+            )
+            ORDER BY hour, minute
+            LIMIT 1
+        )
     )
 )
 AND license_plate IN (

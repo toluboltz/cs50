ac5db99aad4cb001ef19b64c5065ddb449b5a5b0
toluboltz <toluboltz@gmail.com>
2022-05-17 21:16:12 +0000

    Tue, May 17, 2022, 9:16 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Query to see the description of the crime scene report taken on July 28 on Chamberlin Street
+SELECT description
+FROM crime_scene_reports
+WHERE month = 7
+AND day = 28
+AND street = 'Humphrey Street';

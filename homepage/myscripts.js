361280bcdfe5a27a31ad180f473ff25b1ed87135
toluboltz <toluboltz@gmail.com>
2022-06-07 18:05:48 +0000

    Tue, Jun 7, 2022, 6:05 PM +00:00

--- homepage/myscripts.js
+++ homepage/myscripts.js
@@ -1,18 +1,20 @@
 // Run script once DOM content is loaded
 document.addEventListener('DOMContentLoaded', function() {
 
-    var alertPlaceholder = document.getElementById('alert')
+    let alertPlaceholder = document.getElementById('alert');
+    let form = document.getElementById('form');
 
     function alert(message, type) {
-        var wrapper = document.createElement('div')
-        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+        var wrapper = document.createElement('div');
+        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
 
-        alertPlaceholder.append(wrapper)
+        alertPlaceholder.append(wrapper);
     }
 
     // When submit button on the form is clicked
     let submit = document.querySelector('#submit');
     submit.addEventListener('click', function() {
         alert('Nice, you triggered this alert message!', 'success');
+        form.reset();
     })
 })

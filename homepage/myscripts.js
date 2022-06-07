47d86b4defc5221536dd98057e3f8232cfcda0f9
toluboltz <toluboltz@gmail.com>
2022-06-07 18:00:23 +0000

    Tue, Jun 7, 2022, 6:00 PM +00:00

--- homepage/myscripts.js
+++ homepage/myscripts.js
@@ -1,6 +1,13 @@
 // Run script once DOM content is loaded
 document.addEventListener('DOMContentLoaded', function() {
 
+    function alert(message, type) {
+        var wrapper = document.createElement('div')
+        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+        alertPlaceholder.append(wrapper)
+    }
+
     // When submit button on the form is clicked
     let submit = document.querySelector('#submit');
     submit.addEventListener('click', function() {

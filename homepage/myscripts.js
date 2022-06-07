064f4bbfba26cded90f96ba9ec7c5a4380110f37
toluboltz <toluboltz@gmail.com>
2022-06-07 17:51:02 +0000

    Tue, Jun 7, 2022, 5:51 PM +00:00

--- homepage/myscripts.js
+++ homepage/myscripts.js
@@ -1,1 +1,6 @@
-// When submit button on the form is clicked
+// Run script once DOM content is loaded
+document.addEventListener('DOMContentLoaded', function() {
+
+    // When submit button on the form is clicked
+    let submit = document.querySelector
+})

549e644e42509d3422d844239e58a61afd6867cf
toluboltz <toluboltz@gmail.com>
2022-06-07 17:55:15 +0000

    Tue, Jun 7, 2022, 5:55 PM +00:00

--- homepage/myscripts.js
+++ homepage/myscripts.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // When submit button on the form is clicked
     let submit = document.querySelector('.submit');
     submit.addEventListener('click', function() {
-        
+        submit.style.backgroundColor = 'green';
+        doucment.querySelector('#feedback').innerHTML = 'Submitted!';
     })
 })

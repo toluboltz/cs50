a2d5ff1e2814ff5d6f158779acee77fbbaafc342
toluboltz <toluboltz@gmail.com>
2022-06-09 04:07:35 +0000

    Thu, Jun 9, 2022, 4:07 AM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,6 +33,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
+        birthdays = db.execute("SELECT * FROM )
 
         return render_template("index.html")
 

7a31ac8394880759ad5394746b5b0b9cfe0ea6fc
toluboltz <toluboltz@gmail.com>
2022-06-12 05:49:14 +0000

    Sun, Jun 12, 2022, 5:49 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -118,8 +118,17 @@ def quote():
 def register():
     """Register user"""
 
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        return apology("TODO")
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
     return render_template("register.html")
 
 

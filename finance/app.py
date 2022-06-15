09401e425137b50f862fdd4af8791e0847d3d6cf
toluboltz <toluboltz@gmail.com>
2022-06-15 18:11:08 +0000

    Wed, Jun 15, 2022, 6:11 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -78,7 +78,8 @@ def buy():
         user_cash = db.execute("SELECT cash FROM user WHERE id = ?", user_id)
 
         # Ensure the user can afford the purchase
-        if user_cash < (stock["price"] * request.form.get["shares"]
+        if user_cash < (stock["price"] * request.form.get["shares"]):
+            return apology("Can't afford", 400)
 
     # User reached route via GET (as by clicking a link or via redirect)
     return render_template("buy.html")
